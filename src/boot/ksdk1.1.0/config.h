9362b30699fa02b2b1cda41f452525ae2f4814c9
Antros Economides <ae457@cam.ac.uk>
2023-03-17 01:40:58 +0000

    Disable the INA219

--- src/boot/ksdk1.1.0/config.h
+++ src/boot/ksdk1.1.0/config.h
@@ -108,7 +108,7 @@
 	#define		WARP_BUILD_ENABLE_DEVSI7021		0
 	#define		WARP_BUILD_ENABLE_DEVTCS34725		0
 	#define		WARP_BUILD_ENABLE_DEVSSD1331		1
-	#define		WARP_BUILD_ENABLE_DEVINA219		1
+	#define		WARP_BUILD_ENABLE_DEVINA219		0
 #else
 	/*
 	 *	Otherwise: Edit these to set which code paths are activated in the firmware compilation
